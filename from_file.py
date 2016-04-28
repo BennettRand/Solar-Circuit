93582af6bb08fd591dbc4d3bc7425d9cd05c21e2
Ben Rand <brand@solarcity.com>
2016-04-27 18:21:47 -0700

    Filtering Only One Day

--- from_file.py
+++ from_file.py
@@ -14,28 +14,43 @@ from datetime import datetime
 # from measurements import util
 from solar_circuit.libs.tinydb import TinyDB
 from solar_circuit.libs.tinydb.storages import CompressedJSONStorage
+from solar_circuit.libs.tinydb.middlewares import CachingMiddleware
 from solar_circuit.libs.tinydb import Query
 
 _EPOCH = datetime.utcfromtimestamp(0)
 
 def epoch_secs(dt):
-    return float((dt - _EPOCH).total_seconds())
+	return float((dt - _EPOCH).total_seconds())
+
+def ts_to_dt(ts):
+	ts = ts.split('.')[0].rstrip("Z")
+	return datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S")
+
+def ts_hours_ago(ts, hours):
+	now = time.mktime(time.gmtime())
+	ago = now - (hours * 60 * 60)
+	epoch = epoch_secs(ts_to_dt(ts))
+	return ago < epoch
 
 def main():
-    db = TinyDB("./databases/samples.json.bz2", storage=CompressedJSONStorage)
-    for t in db.tables():
-        power_samples = db.table(t).search(Query().channel == "PowerFactor")
-        series = ([], [])
-        for p in power_samples:
-            ts = p['utc'].split('.')[0].rstrip("Z")
-            ts = datetime.strptime(ts, "%Y-%m-%dT%H:%M:%S")
-            series[0].append(ts)
-            series[1].append(p['value'])
-            
-        pyplot.plot(*series)
-    db.close()
-    pyplot.legend(db.tables())
-    pyplot.show()
+	db = TinyDB("./databases/samples.json.bz2",
+				storage=CachingMiddleware(CompressedJSONStorage))
+	for t in db.tables():
+		Sample = Query()
+		power_samples = db.table(t).search(Sample.utc.test(ts_hours_ago, 24) &\
+										   (Sample.channel == "Power"))
+		series = ([], [])
+		for p in power_samples:
+			ts = ts_to_dt(p['utc'])
+			if p['value'] < 0:
+				print ts, p['value'], p['channel']
+			series[0].append(ts)
+			series[1].append(p['value'])
+
+		pyplot.plot(*series)
+	db.close()
+	pyplot.legend(db.tables())
+	pyplot.show()
 	# f_str = open(sys.argv[1], 'rb').read()
 
 	# ts = timeseries.Timeseries.decompress(f_str)
@@ -57,7 +72,7 @@ def main():
 	# 			print hex(resp),
 	# 	time.sleep(.01)
 
-    return
+	return
 
 if __name__ == "__main__":
 	main()
