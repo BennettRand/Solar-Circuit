4bc82be934343f61aac6bfdc77a3977b73f8a5bf
Ben Rand <brand@solarcity.com>
2016-04-30 01:19:10 -0700

    Fixing and Refining Highcharts

--- from_file.py
+++ from_file.py
@@ -37,46 +37,26 @@ def main():
 	db = TinyDB("./databases/samples.json.bz2",
 				storage=CachingMiddleware(CompressedJSONStorage))
 	chart = Chart(title={'text':"Power Measurements"},
-				  yAxis={'title': {'text': "Watts"}})
+				  yAxis={'title': {'text': 'Power (Watts)'}},
+				  xAxis={'type': 'datetime'})
 	for t in [t for t in db.tables() if t != "_default"]:
 		Sample = Query()
 		power_samples = db.table(t).search(Sample.utc.test(ts_hours_ago, 24) &\
 										   (Sample.channel == "Power"))
 		series = [] # ([], [])
 		for p in power_samples:
-			ts = epoch_secs(ts_to_dt(p['utc']))
+			ts = ts_to_dt(p['utc'])
 			if p['value'] < 0:
 				print ts, p['value'], p['channel']
-			series.append((p['utc'], p['value']))
+			else:
+				series.append((ts, p['value']))
 			# series[1].append(p['value'])
 
-		chart.add_data_series(ChartTypes.spline, series, name=t, visible=False,
+		chart.add_data_series(ChartTypes.line, series, name=t, visible=False,
 							  marker={'enabled': False})
 		# pyplot.plot(*series)
 	db.close()
 	chart.show()
-	# pyplot.legend(db.tables())
-	# pyplot.show()
-	# f_str = open(sys.argv[1], 'rb').read()
-
-	# ts = timeseries.Timeseries.decompress(f_str)
-
-	# util.plot_timeseries_with_pyplot(ts)
-	# for x in xrange(0xffff):
-	# 	if x % 0x100 == 0:
-	# 		print "\n[{}]".format(hex(x))
-	# 	rs.sendall(read_registers(x, 1))
-	# 	resp = rs.recv(11)
-	# 	try:
-	# 		resp = struct.unpack("!HHHBBBH", resp)[-1]
-	# 	except struct.error:
-	# 		pass
-	# 	else:
-	# 		if 32 <= resp <= 127:
-	# 			print chr(resp),
-	# 		else:
-	# 			print hex(resp),
-	# 	time.sleep(.01)
 
 	return
 
